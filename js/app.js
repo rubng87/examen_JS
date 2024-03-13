a0828948abdadb66a1291d62e47c6f865fd475fa
“rubng87” <gallegoruben_srye@hotmail.com>
2024-03-13 11:44:06 +0100

    ejercicios

--- js/app.js
+++ js/app.js
@@ -1,48 +1,64 @@
 // Datos de trabajo
 
 const flores = [
-    {nombre: "rosa", color:"rojo", floracion: "primavera", stock: true},
-    {nombre: "rosa", color:"balnco", floracion: "verano", stock: true},
-    {nombre: "jazmin", color:"blanco", floracion: "verano", stock: false},
-    {nombre: "crisantemo", color:"blanco", floracion: "otoño", stock: false},
-    {nombre: "cerezo", color:"blanco", floracion: "primavera", stock: false},
-    {nombre: "clavel", color:"rojo", floracion: "verano", stock: true},
+    {nombre: "rosa", color: "rojo", floracion: "primavera", stock: true},
+    {nombre: "rosa", color: "blanco", floracion: "verano", stock: true},
+    {nombre: "jazmín", color: "blanco", floracion: "verano", stock: false},
+    {nombre: "crisantemo", color: "blanco", floracion: "otoño", stock: false},
+    {nombre: "cerezo", color: "blanco", floracion: "primavera", stock: false},
+    {nombre: "clavel", color: "rojo", floracion: "verano", stock: true},
 ]
 
-flores.sort( (a, b) => {
-    return a.nombre.localeCompare(b.nombre, "es-ES", {numeric: true});
+flores.sort( (a, b) => { 
+    return a.nombre.localeCompare(b.nombre, "es-ES", {numeric: true});   
 })
-
 
 // console.log(flores);
 
-// Tiene que mostrasrse en el HTML los datos de las flores
+// Tiene que mostrarse en el HTML los datos de las flores
 // de esta manera:
 // Como lista
-// Flor: rosa, de color rojo, florece e primavera y tenemos stock.
+// Flor: rosa, de color rojo, florece en primavera y tenemos stock
 
-let ejercicio1 = document.getElementById('ejercicio1');
+let ejercicio1 = document.getElementById("ejercicio1");
 let html1 = "<ul>";
 
-
-//for clásico
+// for clásico
 // for (let i = 0; i < flores.length; i++) {
 //     console.log(flores[i]);
 // }
 
-flores.forEach(flor => {
+flores.forEach( flor => {
     let textoStock = ""
     if (!flor.stock) {
         textoStock = "no ";
     } 
-    html1 += `<li>Flor: ${flor.nombre}, de color ${flor.color}, florece en ${flor.floracion} y ${textoStock}tenemos stock.</li>`;
-
+    html1 += `<li>Flor: ${flor.nombre}, de color ${flor.color}, florece en ${flor.floracion} y ${textoStock}tenemos stock</li>`;
 })
 
 html1 += "</ul>";
-
-
-
-
-
 ejercicio1.innerHTML = html1;
+
+// ==============================================================================
+
+// Listar las flores de color blanco que florecen en verano
+// Modelo de mensaje de salida:
+// Flor: rosa, de color blanco, florece en verano y tenemos stock
+// se mostrará el resultado en #ejercicio2
+
+let ejercicio2 = document.getElementById("ejercicio2");
+let html2 = "<ul>";
+
+flores.forEach((flor) => {
+  let textoStock = "";
+  if (!flor.stock) {
+    textoStock = "no ";
+  }
+  if (flor.color === "blanco" && flor.floracion === "verano") {
+    html2 += `<li>Flor: ${flor.nombre}, de color ${flor.color}, florece en ${flor.floracion} y ${textoStock}tenemos stock</li>`;
+  }
+});
+
+html2 += "</ul>";
+ejercicio2.innerHTML = html2;
+
